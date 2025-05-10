a5d4ace65659be42e6c2425cbd11dd53d972cb14
Bhushan Chaudhari <chaudharib0506@gmail.com>
2025-05-10 13:09:52 +0900

    intial commit

---
+++ src/main/java/com/chat/app/model/ChatMessage.java
@@ -0,0 +1,31 @@
+package com.chat.app.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatMessage {
+	private Long id;
+	private String sender;
+	private String content;
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
